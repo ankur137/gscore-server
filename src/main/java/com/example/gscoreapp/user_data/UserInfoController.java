1c56a6aa058ba6cc653e0cac2218c91c0f6f12e6
Shreeyash <shreeyashyende@gmail.com>
2021-03-25 01:17:52 -0400

    user id api done

---
+++ src/main/java/com/example/gscoreapp/user_data/UserInfoController.java
@@ -0,0 +1,49 @@
+package com.example.gscoreapp.user_data;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+
+@RestController
+public class UserInfoController {
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    private static final HttpClient client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+
+
+    @RequestMapping("/gituser/{username}")
+    public void addUser(@PathVariable String username) throws ExecutionException, InterruptedException, JsonProcessingException {
+        System.out.println("Api hit");
+        HttpRequest req = HttpRequest.newBuilder(URI.create("https://api.github.com/users/" + username))
+                .GET().build();
+
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, HttpResponse.BodyHandlers.ofString());
+
+        response.thenAccept(res -> System.out.println(res));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        UserInfo user = objectMapper.readValue(response.get().body(), UserInfo.class);
+        userInfoService.addUser(user);
+    }
+
+//    @GetMapping("/test")
+//    public String hi() {
+//        return "HI";
+//    }
+
+}
+
---
+++ src/main/java/com/example/gscoreapp/user_data/UserInfoRepository.java
@@ -0,0 +1,6 @@
+package com.example.gscoreapp.user_data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserInfoRepository extends CrudRepository<UserInfo, Integer> {
+}
