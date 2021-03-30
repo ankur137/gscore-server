513c394053573d0e83576dfab78295fde1d979c4
meet mehta <meetmehta10@gmail.com>
2021-03-30 19:29:48 -0400

    sql file updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,12 +16,46 @@ CREATE TABLE github_user_basic_info (
 insert into github_user_basic_info(uid, username, gid, name, avatar_url, followers, following, location, created_at, updated_at, public_repos, public_gists)
 values (2, 'sdgfbhdhrg', 2, 'qwerty', 'abc.com/sahfdsja', 23, 443, 'Mumbai', '8/3/3', '3/2/4', 2, 4);
 
-github_user_basic_info alter column avatar_url type varchar(150);
+alter table github_user_basic_info alter column avatar_url type varchar(150);
 
-github_user_basic_info alter column "location" type varchar(50);
+alter table github_user_basic_info alter column "location" type varchar(50);
 
-github_user_basic_info alter column created_at type varchar(50);
+alter table github_user_basic_info alter column created_at type varchar(50);
 
-github_user_basic_info alter column updated_at type varchar(50);
+alter table github_user_basic_info alter column updated_at type varchar(50);
+
+alter table github_user_basic_info add primary key (uid);
 
 commit;
+
+CREATE TABLE github_user_repo (
+        urid integer primary key ,
+        uid integer,
+        gid integer,
+        is_forked BIT,
+        commits integer ,
+        pulls integer,
+        releases integer,
+        issues integer,
+        contributors integer,
+        size integer,
+        forks integer,
+        stargazers_count integer,
+        watchers integer,
+        open_issues integer,
+        foreign key (uid) REFERENCES  github_user_basic_info(uid)
+);
+
+CREATE TABLE github_user_repo_code(
+        urcid integer primary key,
+        urid integer,
+        uid integer,
+        gid integer,
+        programming_language varchar(25),
+        lines_of_code_repo integer,
+        comment_repo varchar(50),
+        file_size integer,
+        files_by_language varchar(50),
+        code_language varchar(20),
+        foreign key (urid) REFERENCES  github_user_repo(urid)
+);
